ad6cfb1fbed6314996fea7c8e886e6e24b84804c
wpawel <pawel.wojcik@gmail.com>
2018-04-25 11:03:15 +0200

    change IP detector (CloudFlare fix)
    CloudFlare support for ip detector

--- src/Tool/IpDetector.php
+++ src/Tool/IpDetector.php
@@ -18,29 +18,47 @@ abstract class IpDetector
      *
      * @return string
      */
+ 
     public static function detect(Configuration $config)
-    {
-        if(self::$ipAddress === null) {
-            if (getenv('HTTP_CLIENT_IP')) {
-                self::$ipAddress = getenv('HTTP_CLIENT_IP');
-            } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
-                self::$ipAddress = getenv('HTTP_X_FORWARDED_FOR');
-            } elseif (getenv('HTTP_X_FORWARDED')) {
-                self::$ipAddress = getenv('HTTP_X_FORWARDED');
-            } elseif (getenv('HTTP_FORWARDED_FOR')) {
-                self::$ipAddress = getenv('HTTP_FORWARDED_FOR');
-            } elseif (getenv('HTTP_FORWARDED')) {
-                self::$ipAddress = getenv('HTTP_FORWARDED');
-            } elseif (getenv('REMOTE_ADDR')) {
-                self::$ipAddress = getenv('REMOTE_ADDR');
-            } else {
-                self::$ipAddress = 'UNKNOWN';
-            }
-            if (self::$ipAddress === '0:0:0:0:0:0:0:1' || self::$ipAddress === '::1') {
-                self::$ipAddress = $config::LOCAL_IP;
-            }
-        }
-
-        return self::$ipAddress;
-    }
+		{	
+			self::$ipaddress = '';
+			 
+			if (function_exists('apache_request_headers')) {
+				$headers = apache_request_headers();
+			} else {
+				$headers = $_SERVER;
+			}
+			// CloudFlare support
+			if (array_key_exists('HTTP_CF_CONNECTING_IP', $headers)) {
+				// Validate IP address (IPv4/IPv6)
+				if (filter_var($headers['HTTP_CF_CONNECTING_IP'], FILTER_VALIDATE_IP)) 
+				{
+					self::$ipaddress = $headers['HTTP_CF_CONNECTING_IP']; 
+					return self::$ipaddress;   
+				}
+			}
+			if (array_key_exists('X-Forwarded-For', $headers)) {
+				$_SERVER['HTTP_X_FORWARDED_FOR'] = $headers['X-Forwarded-For'];
+			}
+			if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && (!isset($_SERVER['REMOTE_ADDR'])
+				|| preg_match('/^127\..*/i', trim($_SERVER['REMOTE_ADDR'])) || preg_match('/^172\.16.*/i', trim($_SERVER['REMOTE_ADDR']))
+				|| preg_match('/^192\.168\.*/i', trim($_SERVER['REMOTE_ADDR'])) || preg_match('/^10\..*/i', trim($_SERVER['REMOTE_ADDR'])))) {
+				if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')) {
+					$ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+					self::$ipaddress = $ips[0];
+				} else {
+					self::$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+				}
+			} else {
+				self::$ipaddress = $_SERVER['REMOTE_ADDR'];
+			}
+			
+			if(self::$ipaddress === '0:0:0:0:0:0:0:1' || self::$ipaddress === '::1') {
+				self::$ipaddress = $config::LOCAL_IP;
+			}		
+			
+			return self::$ipaddress;
+		}
+	
+	
 }
